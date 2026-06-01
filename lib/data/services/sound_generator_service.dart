5809e22aa2b638adc9c22c74fe173b8966962a75
zxrcode <zadxarring@bk.ru>
2026-06-01 22:33:09 +0500

    fix web version

--- lib/data/services/sound_generator_service.dart
+++ lib/data/services/sound_generator_service.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/foundation.dart';
 import 'package:sound_generator/sound_generator.dart';
 import 'package:sound_generator/waveTypes.dart';
 
@@ -7,24 +8,26 @@ class SoundGeneratorService {
   final int _sampleRate = 44100;
 
   Future<void> init() async {
+    if (kIsWeb) return; // sound_generator does not support web
     await SoundGenerator.init(_sampleRate);
     SoundGenerator.setWaveType(waveTypes.SINUSOIDAL);
     SoundGenerator.setVolume(0.5); // Start with safe volume
   }
 
   void start() {
-    if (_isPlaying) return;
+    if (kIsWeb || _isPlaying) return;
     SoundGenerator.play();
     _isPlaying = true;
   }
 
   void stop() {
-    if (!_isPlaying) return;
+    if (kIsWeb || !_isPlaying) return;
     SoundGenerator.stop();
     _isPlaying = false;
   }
 
   void setFrequency(double freq) {
+    if (kIsWeb) return;
     _frequency = freq;
     SoundGenerator.setFrequency(_frequency);
   }
